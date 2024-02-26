56dd7a2f8d6a2bb1bf761ce4d8e2e30ac64affed
kennedy <x.johnkennedy.x@gmail.com>
2024-02-26 19:38:35 +0300

    Add Button, TravelForm, and TripSelection components

---
+++ src/app/components/TravelForm.tsx
@@ -0,0 +1,99 @@
+import React, { useReducer } from 'react';
+import DropDownSelect from './DropDownSelect';
+import { CiLocationOn } from 'react-icons/ci';
+import CustomDatePicker from './CustomDatepicker';
+import Button from './Button';
+
+const sampleLocation = [
+  { id: 101, value: 'Nairobi' },
+  { id: 102, value: 'Nakuru' },
+];
+
+type State = {
+  departureDate: Date | null;
+  returnDate: Date | null;
+  departure: number | null;
+  destination: number | null;
+};
+
+type Action =
+  | { type: 'setDepartureDate'; payload: Date | null }
+  | { type: 'setReturnDate'; payload: Date | null }
+  | { type: 'setDeparture'; payload: number | null }
+  | { type: 'setDestination'; payload: number | null };
+
+const initialState = {
+  departureDate: null,
+  returnDate: null,
+  departure: null,
+  destination: null,
+};
+
+function reducer(state: State, action: Action) {
+  switch (action.type) {
+    case 'setDepartureDate':
+      return { ...state, departureDate: action.payload };
+    case 'setReturnDate':
+      return { ...state, returnDate: action.payload };
+    case 'setDeparture':
+      return { ...state, departure: action.payload };
+    case 'setDestination':
+      return { ...state, destination: action.payload };
+    default:
+      throw new Error();
+  }
+}
+
+export default function TravelForm({ returnTrip }: { returnTrip: boolean }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleClick = () => {
+    console.log(state);
+  };
+
+  return (
+    <div className="flex flex-row gap-20 bg-white p-4 shadow-md">
+      <div>
+        <DropDownSelect
+          handleSelect={(id) => dispatch({ type: 'setDeparture', payload: id })}
+          data={sampleLocation}
+          Icon={CiLocationOn}
+          text="Traveling From"
+        />
+      </div>
+
+      <div>
+        <DropDownSelect
+          handleSelect={(id) => dispatch({ type: 'setDestination', payload: id })}
+          data={sampleLocation}
+          Icon={CiLocationOn}
+          text="Destination"
+        />
+      </div>
+
+      <div>
+        <CustomDatePicker
+          selectedDate={state.departureDate}
+          handleDateChange={(date) => dispatch({ type: 'setDepartureDate', payload: date })}
+          text="Departure Date"
+        />
+      </div>
+
+      {returnTrip && (
+        <div>
+          <CustomDatePicker
+            selectedDate={state.returnDate}
+            handleDateChange={(date) => dispatch({ type: 'setReturnDate', payload: date })}
+            text="Return Date"
+          />
+        </div>
+      )}
+
+      <div className="flex items-center">
+        <Button type="primary" onClick={handleClick}>
+          Find Bus
+        </Button>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/components/TripSelection.tsx
@@ -0,0 +1,38 @@
+'use client';
+import { useState } from 'react';
+import Button from './Button';
+import TravelForm from './TravelForm';
+
+export default function TripSelection() {
+  const [isOneWaySelected, setOneWaySelected] = useState(true);
+  const [isRoundTripSelected, setRoundTripSelected] = useState(false);
+  const [returnTrip, setReturnTrip] = useState(false);
+
+  const handleOneWayClick = () => {
+    setOneWaySelected(true);
+    setRoundTripSelected(false);
+    setReturnTrip(false);
+  };
+
+  const handleRoundTripClick = () => {
+    setOneWaySelected(false);
+    setRoundTripSelected(true);
+    setReturnTrip(true);
+  };
+
+  return (
+    <div className="flex justify-center w-full">
+      <div className="flex flex-col">
+        <div className="flex flex-row">
+          <Button type={'variable'} onClick={handleOneWayClick} selected={isOneWaySelected}>
+            One Way
+          </Button>
+          <Button type={'variable'} onClick={handleRoundTripClick} selected={isRoundTripSelected}>
+            Round Trip
+          </Button>
+        </div>
+        <TravelForm returnTrip={returnTrip} />
+      </div>
+    </div>
+  );
+}
